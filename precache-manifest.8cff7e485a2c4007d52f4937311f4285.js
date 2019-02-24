04b41e3e0c237c7400352deb3762fd0c5f10c9e5
Dragos Dehelean <dragos.dehelean@selenis.ro>
2019-02-25 01:21:09 +0200

    Updates

---
+++ precache-manifest.8cff7e485a2c4007d52f4937311f4285.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "be92ab00f0f555419adb491aa392b9ca",
+    "url": "/React-RSVP-App/static/media/header-bg.be92ab00.jpg"
+  },
+  {
+    "revision": "db1cfe5b962f58f0f54c",
+    "url": "/React-RSVP-App/static/js/runtime~main.db1cfe5b.js"
+  },
+  {
+    "revision": "e7ff2cf88ee582750a99",
+    "url": "/React-RSVP-App/static/js/main.e7ff2cf8.chunk.js"
+  },
+  {
+    "revision": "3c6e310ec269c012f852",
+    "url": "/React-RSVP-App/static/js/2.3c6e310e.chunk.js"
+  },
+  {
+    "revision": "e7ff2cf88ee582750a99",
+    "url": "/React-RSVP-App/static/css/main.4df40314.chunk.css"
+  },
+  {
+    "revision": "07bd30968fd63988bb24d81aaffdbbed",
+    "url": "/React-RSVP-App/index.html"
+  }
+];
